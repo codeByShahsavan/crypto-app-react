d903b1199709c87dcc15af1f0cb7ebda0f029bb6
fatemeh shahsavan <shahsavan051@yahoo.com>
2026-01-05 18:18:37 +0330

    currency component

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -2,7 +2,7 @@ import chartUp from "../../assets/chart-up.svg"
 import chartDown from "../../assets/chart-down.svg"
 import { RotatingLines } from 'react-loader-spinner'
 import styles from "./TableCoin.module.css"
-function TableCoin({coins,isLoading}) {
+function TableCoin({coins,isLoading,currency}) {
   console.log(coins)
   return (
     <div className={styles.container}>
@@ -20,7 +20,7 @@ function TableCoin({coins,isLoading}) {
             </thead>
             <tbody>
                 {coins.map((coin)=>(
-                   <TableRow key={coin.id} coin={coin}/>
+                   <TableRow key={coin.id} coin={coin} currency={currency}/>
                 ))}
             </tbody>
         </table>
@@ -37,7 +37,8 @@ const TableRow=({coin:{
     symbol,
     current_price,
     price_change_percentage_24h:price_change
-    ,total_volume}})=>{
+    ,total_volume
+},currency})=>{
         return(
          <tr >
                         <td>
@@ -47,7 +48,7 @@ const TableRow=({coin:{
                             </div>
                         </td>
                         <td>{name}</td>
-                        <td>${current_price.toLocaleString()}</td>
+                        <td>{currency === "usd"?"$":currency==="eur"?"€":"¥"}{current_price.toLocaleString()}</td>
                         <td className={price_change>0?styles.success:styles.error}> {price_change != null ? price_change.toFixed(2) : "0.00"}%</td>
                         <td>{total_volume.toLocaleString()}</td>
                         <td>
