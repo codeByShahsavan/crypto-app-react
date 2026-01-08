52f75ab42f8f36ba2951f2f7f7b963691d706738
fatemeh shahsavan <shahsavan051@yahoo.com>
2026-01-08 11:46:45 +0330

    change format data api

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react"
+import { ConvertData } from "../../helpers/ConvertData"
 import styles from "./Chart.module.css"
 
 function Chart({chart,setChart}) {
+    const [type,setType]=useState("prices") 
+    console.log(ConvertData(chart,type))
   return (
     <div className={styles.container}>
         <span className={styles.cross} onClick={()=>setChart(null)}>X</span>
         <div className={styles.chart}></div>
-
+        
         </div>
   )
 }
